b36f4845b7813f3310f46c9abe0881be158f4a55
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-03-26 21:19:51 +0400

    добавил проверку правильной инициализации категории

--- tests/test_category.py
+++ tests/test_category.py
@@ -16,11 +16,21 @@ def test_count_category(create_category_book, create_category_toy):
     assert category1.count_products == 6
 
 
-def test_init_category(create_category_toy):
+def test_init_category(create_category_toy, create_category_smartphone,
+                       create_category_grass):
     """ Проверяет инициализацию класса Category. """
     assert create_category_toy.name == 'Игрушки'
     assert create_category_toy.description == 'Сфера развлечений'
     assert create_category_toy._get_products()[0].name == 'мяч'
+
+    assert create_category_smartphone.name == 'Смартфоны'
+    assert create_category_grass.name == 'Трава газонная'
+
+
+def test_init_category_error():
+    """ Проверяет попытку неправильной инициализации категории. """
+    with pytest.raises(TypeError):
+        assert Category('test_name', 'test_description', 'test_str')
 
 
 def test_add_products(create_category_book):
